55bb86b3fffafbc046f132ea9b4f30e79464d1cc
Evan Meshberg <emeshberge@gmail.com>
2018-08-02 02:44:10 +0000

    Lesson 5

---
+++ version_5/for_loops.js
@@ -0,0 +1,11 @@
+for(var i = 0; i < 3; i++){
+    console.log(i); 
+}
+
+var testArray = ['item 1', 'item 2', 'item 3'];
+
+for(var i = 0; i < testArray.length; i++){
+    console.log(testArray[i]); 
+}
+
+
